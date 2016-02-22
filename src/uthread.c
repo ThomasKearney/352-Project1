a4fad118961297516d3800972702bfae0aec4000
ThomasKearney <thomasnkearney@gmail.com>
2016-02-22 15:01:07 -0600

    skelly

---
+++ src/uthread.c
@@ -0,0 +1,26 @@
+
+
+void system_init()
+{
+	
+}
+
+int uthread_create(void (*func)(), int priority)
+{
+	
+}
+
+void uthread_yield()
+{
+	
+}
+
+void uthread_exit()
+{
+	
+}
+
+int uthread_send(int tid, void* content, int size)
+{
+	
+}
